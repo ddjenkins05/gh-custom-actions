517cdfdf489e32c9ad473ac92ee54efc9f7dacc1
Doug Jenkins <ddjenkins05@yahoo.com>
2026-09-15 06:41:57 -0700

    trying to fix java

--- .github/actions/cached-deps/deploy-s3-java/main.js
+++ .github/actions/cached-deps/deploy-s3-java/main.js
@@ -1,10 +1,11 @@
-const core = require('@actions/core');
-const exec = require('@actions/exec');
-const github = require('@actions/github');
+(async () => {
+    const core = await import('@actions/core');
+    const exec = await import('@actions/exec');
+    const github = await import('@actions/github');
 
-function run() {
-    core.notice('Hello from my custom Java action');
+    function run() {
+        core.notice('Hello from my custom Java action');
+    }
 
-}
-
-run();
+    run();
+})();
